07d7813c58e295e8c95590a197310458d96fda25
ArtificialBlue <oceantraveler201@gmail.com>
2021-02-02 01:39:09 -0600

    Cleaned Up Code

--- Playlist-Linked-List/Playlist.py
+++ Playlist-Linked-List/Playlist.py
@@ -9,13 +9,6 @@ class Playlist:
     newest_song.set_next_song(self.__first_song)
     self.__first_song = newest_song
 
-
-
-  # TODO: Create a method called find_song that searches for whether a song exits
-  # in the playlist and returns its index. The method has one parameters, title, 
-  # which is the title of the song to be searched for. If the song is found, 
-  # return its index.
-
   def find_song(self, title):
     current_song = self.__first_song
     index = 0
@@ -28,9 +21,6 @@ class Playlist:
         current_song = current_song.get_next_song()
     index = -1
     return index
-
-
-  # TODO: Create a method called remove_song that removes a song from the playlist. This method takes one parameter, title, which is the song that should be removed. 
 
   def remove_song(self, title):
     previous = None
@@ -51,17 +41,10 @@ class Playlist:
           print("Last Song selected and removed.")
           break
         else:
+        #Middle Case
           previous.set_next_song(current.get_next_song())
           print("Centered Song selected and removed.")
           break
-
-        
-
-
-        
-
-
-  # TODO: Create a method called length, which returns the number of songs in the playlist.
 
   def length(self):
     length = 0
@@ -71,15 +54,6 @@ class Playlist:
       length += 1
       current = current.get_next_song()
     return length
-
-
-
-  # TODO: Create a method called print_songs that prints a numbered list of the songs in the playlist.
-
-  # Example:
-  # 1. Song Title 1
-  # 2. Song Title 2
-  # 3. Song Title 3
 
   def print_songs(self):
     current = self.__first_song
